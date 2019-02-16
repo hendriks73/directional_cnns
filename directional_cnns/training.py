d16cd42a59f7842260e2fd0f5fc9fc79563b38ac
Hendrik Schreiber <hs@tagtraum.com>
2019-02-16 09:30:07 +0100

    Extracted loading functions.

--- directional_cnns/training.py
+++ directional_cnns/training.py
@@ -4,11 +4,9 @@ Main entry point to train multiple models and predict on test sets.
 
 import argparse
 import datetime
-import math
 import time
 from os.path import join
 
-import numpy as np
 import tensorflow as tf
 from sklearn.externals import joblib
 from sklearn.preprocessing import OneHotEncoder
@@ -16,10 +14,10 @@ from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.python.keras.models import load_model
 from tensorflow.python.keras.optimizers import Adam
-from tensorflow.python.keras.preprocessing.image import apply_transform
 from tensorflow.python.lib.io.file_io import FileIO, file_exists
 
 from directional_cnns.cloudml_utils import create_local_copy, save_model, make_missing_dirs
+from directional_cnns.loader import create_mel_sample_loader, create_cq_sample_loader
 from directional_cnns.prediction import predict
 from directional_cnns.generator import DataGenerator, tempo_augmenter, key_augmenter
 from directional_cnns.groundtruth import TempoGroundTruth, KeyGroundTruth
@@ -27,97 +25,6 @@ from directional_cnns.models import ModelLoader
 from directional_cnns.network.shallow import create_shallow_key_model, create_shallow_tempo_model
 from directional_cnns.network.vgg import create_vgg_like_model
 from directional_cnns.normalizer import std_normalizer
-
-
-def create_mel_sample_loader(dataset, shape=(40, 256, 1), random_offset=True, normalizer=None):
-    """
-    Create sample loading function that is capable of loading and augmenting a sample
-    based on an augmentation parameter. The loader function is suitable for the Mel spectrograms
-    we use for tempo estimation.
-
-    :param normalizer: normalization function
-    :param shape: desired shape
-    :param random_offset: return the first X frames or randomly chosen X frames, X=shape[1]
-    :param dataset: dataset, dict that maps a ``key`` to a sample
-    :return: function that returns a sample for a key (or sample id)
-    """
-    def mel_sample_loader(key, augmentation_parameter=1.):
-        scale_factor = augmentation_parameter
-        if scale_factor is None:
-            scale_factor = 1.
-        mel_spectrogram = dataset[key]
-        # we want (40, length)
-        offset = 0
-        length = shape[1]
-        max_length_after_scaling = max(length, math.ceil(length * scale_factor))
-        if mel_spectrogram.shape[1] < max_length_after_scaling:
-            # the source is too short, let's pad with zeros
-            # better with np.pad()?
-            copy = np.zeros((mel_spectrogram.shape[0], max_length_after_scaling, mel_spectrogram.shape[2]))
-            copy[:, :mel_spectrogram.shape[1], :] = mel_spectrogram
-            mel_spectrogram = copy
-        if random_offset and max_length_after_scaling < mel_spectrogram.shape[1]:
-            # only compute random offset, if we really have material to work with
-            offset = np.random.randint(0, mel_spectrogram.shape[1] - max_length_after_scaling)
-        if scale_factor is not None and scale_factor != 1.:
-            unscaled = mel_spectrogram[:, offset:offset + max_length_after_scaling]
-            data = scale(unscaled, scale_factor)
-        else:
-            data = mel_spectrogram[:, offset:offset + length]
-        if normalizer is not None:
-            data = normalizer(data)
-
-        return data
-
-    def scale(mel_spectrogram, scale_factor):
-        w = mel_spectrogram.shape[0]
-        h = mel_spectrogram.shape[1]
-        transform = np.array([[scale_factor, 0, 0], [0, 1, 0], [0, 0, 1]])
-        image = mel_spectrogram.astype(np.float64).reshape(w, h)
-        scaled_image = apply_transform(image, transform, fill_mode='constant').astype(np.float16)
-        # if the result is shorter, shorten the np array as well
-        if scale_factor > 1:
-            new_length = int(scaled_image.shape[1] / scale_factor)
-            scaled_image = scaled_image[:, :new_length, ]
-        mel_spectrogram = np.expand_dims(scaled_image.astype(mel_spectrogram.dtype), axis=2)
-        return mel_spectrogram
-
-    return mel_sample_loader
-
-
-def create_cq_sample_loader(dataset, shape=(168, 60, 1), random_offset=True, normalizer=None):
-    """
-    Create sample loading function that is capable of loading and augmenting a sample
-    based on an augmentation parameter. The loader function is suitable for the constant Q spectrograms
-    we use for key estimation.
-
-    :param normalizer: normalization function
-    :param shape: desired shape
-    :param random_offset: return the first X frames or randomly chosen X frames, X=shape[1]
-    :param dataset: dataset, dict that maps a ``key`` to a sample
-    :return: function that returns a sample for a key (or sample id)
-    """
-    def cq_sample_loader(key, augmentation_parameter=0):
-        if augmentation_parameter is None:
-            bin_shift = 0
-        else:
-            bin_shift = augmentation_parameter * 2  # , because we have two bins per semitone
-        cq_spectrogram = dataset[key]
-        offset = 0
-        length = shape[1]
-        if random_offset and cq_spectrogram.shape[1] > length:
-            offset = np.random.randint(0, cq_spectrogram.shape[1] - length)
-        if bin_shift is not None:
-            # possible shifts: -4 ... +7 -> -8 ... +14 (two bins per semitone)
-            data = cq_spectrogram[8+bin_shift:-(16-bin_shift), offset:offset + length]
-        else:
-            data = cq_spectrogram[8:-16, offset:offset + length]
-        if normalizer is not None:
-            data = normalizer(data)
-
-        return data
-
-    return cq_sample_loader
 
 
 def train_and_predict(train_file, valid_file, test_files, feature_files, job_dir, model_dir):
